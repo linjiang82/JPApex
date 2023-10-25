94846e103d44d79700b53d36dc0d6d05232187e9
jiang.lin <jiang.lin@ladbrokes.com.au>
2023-10-26 08:48:09 +1000

    Initial Commit

---
+++ JPApex/JPApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexApp.swift
+//  JPApex
+//
+//  Created by Jiang Lin on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct JPApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
